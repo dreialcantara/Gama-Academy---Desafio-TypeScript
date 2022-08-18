bf75c3309e4901573361b155da68b679c3f55d84
dreialcantara <andreialcantara@id.uff.br>
2022-08-18 12:54:11 -0300

    testes1

--- testes.js
+++ testes.js
@@ -1,7 +1,7 @@
 "use strict";
 // MÉDIA ARITMETICA DE ARRAY
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeChar = exports.checkForFactor = exports.findAverage = void 0;
+exports.quarterOf = exports.square = exports.removeChar = exports.checkForFactor = exports.findAverage = void 0;
 function findAverage(array) {
     var soma = 0;
     let i;
@@ -33,3 +33,25 @@ function removeChar(str) {
     return str;
 }
 exports.removeChar = removeChar;
+function square(a) {
+    return a * a;
+}
+exports.square = square;
+//  qual trimestre do ano é o mes
+function quarterOf(month) {
+    if (month >= 1 && month <= 3) {
+        return 1;
+    }
+    else if (month >= 4 && month <= 6) {
+        return 2;
+    }
+    else if (month >= 7 && month <= 9) {
+        return 3;
+    }
+    else if (month >= 10 && month <= 12) {
+        return 4;
+    }
+    else
+        return 0;
+}
+exports.quarterOf = quarterOf;
--- testes.ts
+++ testes.ts
@@ -1,26 +1,48 @@
 // MÉDIA ARITMETICA DE ARRAY
 
 export function findAverage(array: number[]): any {
-    var soma = 0
-    let i: number
-    var vazio:string = "O array está vazio!"
-    
-    if (array.length === 0) { return vazio  } else 
-    for (i = 0; i < array.length; i++) 
-    {soma += array[i];}
-      return soma/array.length;
+  var soma = 0;
+  let i: number;
+  var vazio: string = "O array está vazio!";
+
+  if (array.length === 0) {
+    return vazio;
+  } else
+    for (i = 0; i < array.length; i++) {
+      soma += array[i];
     }
+  return soma / array.length;
+}
 
 // Verificando se número é fatorável
-    export function checkForFactor(base: number, factor: number):boolean {
-        if (factor % base === 0) { return true}
-        else {return false}
-      }
+export function checkForFactor(base: number, factor: number): boolean {
+  if (factor % base === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // Retirar primeiro e ultimo caractere da string
-      export function removeChar(str: string): string {
-        str = str.slice(1);
-        str = str.slice(-1);
-        return str
-      }
-      
+export function removeChar(str: string): string {
+  str = str.slice(1);
+  str = str.slice(-1);
+  return str;
+}
+
+export function square(a: number): number {
+  return a * a;
+}
+
+//  qual trimestre do ano é o mes
+export function quarterOf(month: number): number {
+  if (month >= 1 && month <= 3) {
+    return 1;
+  } else if (month >= 4 && month <= 6) {
+    return 2;
+  } else if (month >= 7 && month <= 9) {
+    return 3;
+  } else if (month >= 10 && month <= 12) {
+    return 4;
+  } else return 0;
+}
